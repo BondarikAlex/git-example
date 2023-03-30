020e263f077a0bed9fb12fced85d93169d004579
BondarikAlex <129206388+bondarikalex@users.noreply.github.com>
2023-03-30 23:51:11 +0300

    Initial fommit

--- src/Formula_HD/Ingridiendts.java
+++ src/Formula_HD/Ingridiendts.java
@@ -1,4 +1,13 @@
 package Formula_HD;
 
 public enum Ingridiendts {
+    MAYONNAISE(1,13),KETCHUP(1,10),MUSTARD(1,11);
+
+    final int dosa;
+    final int price;
+
+    Ingridiendts(int dosa, int price) {
+        this.dosa = dosa;
+        this.price = price;
+    }
 }
