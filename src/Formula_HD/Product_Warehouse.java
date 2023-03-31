af86f0439f2df9e6a5d2763491d3220fa5f0ee64
Bondarik Alex <alexbondarik36@gmail.com>
2023-03-31 16:24:46 +0300

    Изменение 1

--- src/Formula_HD/Product_Warehouse.java
+++ src/Formula_HD/Product_Warehouse.java
@@ -14,9 +14,67 @@ package Formula_HD;
         private static Integer Chile = 1000;//чили
         private static Integer PickledCucumber = 1000;//соленные огурцы
 
+        public static Integer getHD() {
+            return HD;
+        }
 
+        public static void setHD(Integer HD) {
+            Product_Warehouse.HD = HD;
+        }
 
+        public static Integer getMayonnaise() {
+            return Mayonnaise;
+        }
 
+        public static void setMayonnaise(Integer mayonnaise) {
+            Mayonnaise = mayonnaise;
+        }
 
+        public static Integer getKetchup() {
+            return Ketchup;
+        }
 
+        public static void setKetchup(Integer ketchup) {
+            Ketchup = ketchup;
+        }
+
+        public static Integer getMustard() {
+            return Mustard;
+        }
+
+        public static void setMustard(Integer mustard) {
+            Mustard = mustard;
+        }
+
+        public static Integer getSweetOnions() {
+            return SweetOnions;
+        }
+
+        public static void setSweetOnions(Integer sweetOnions) {
+            SweetOnions = sweetOnions;
+        }
+
+        public static Integer getHalapenu() {
+            return Halapenu;
+        }
+
+        public static void setHalapenu(Integer halapenu) {
+            Halapenu = halapenu;
+        }
+
+        public static Integer getChile() {
+            return Chile;
+        }
+
+        public static void setChile(Integer chile) {
+            Chile = chile;
+        }
+
+        public static Integer getPickledCucumber() {
+            return PickledCucumber;
+        }
+
+        public static void setPickledCucumber(Integer pickledCucumber) {
+            PickledCucumber = pickledCucumber;
+        }
     }
